d80e01e5aacf73f13559f5cb641c7245b895b39b
icchigoo <icchigoo12@gmail.com>
2023-06-01 03:57:16 +0545

    Category post method

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -26,4 +17,10 @@ Route::prefix('admin')->middleware(['auth','isAdmin'])->group(function(){
     
     Route::get('dashboard',[App\Http\Controllers\Admin\DashboardController::class, 'index'] );
 
+
+    //Category Routes 
+
+    Route::get('category', [App\Http\Controllers\Admin\CategoryController::class, 'index']);
+    Route::get('category/create', [App\Http\Controllers\Admin\CategoryController::class, 'create']);
+    Route::post('category', [App\Http\Controllers\Admin\CategoryController::class, 'store']);
 });
